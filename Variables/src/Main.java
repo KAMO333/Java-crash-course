cd4b428138030cd330df874c12f838062c310b2d
@kammopajhb024 <mmopanemoroka@gmail.com>
2024-12-17 14:13:23 +0200

    learned variables

---
+++ Variables/src/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+//        String name = "Kamogelo";
+//        int age = 32;
+//        System.out.println(name + " is " + age + " years old.");
+//
+//        Storing numbers.
+
+//        int max =  Integer.MAX_VALUE;
+//        int min = Integer.MIN_VALUE;
+//
+//        System.out.println("The maximum value for int is " + max );
+//        System.out.println("The minimum value for int is " + min );
+//
+//        byte maxByteValue = Byte.MAX_VALUE;
+//        byte minByteValue = Byte.MIN_VALUE;
+//
+//        System.out.println("The maximum byte value is " + maxByteValue );
+//        System.out.println("The minimum byte value is " + minByteValue );
+//
+//        long maxLongValue = Long.MAX_VALUE;
+//        long minLongValue = Long.MAX_VALUE;
+//
+//        System.out.println("The maximum long value is " + maxLongValue );
+//        System.out.println("The minimum long value is " + minLongValue );
+
+//        short shortMaxValue = Short.MAX_VALUE;
+//        short shortMinValue = Short.MIN_VALUE;
+//
+//        System.out.println("The maximum short value is " + shortMaxValue );
+//        System.out.println("The minimum short value is " + shortMinValue );
+
+//        float number = 3.14f;
+//        double digit = 3.14;
+//
+//        System.out.println(number);
+//        System.out.println(digit);
+//
+//        float maxFloat = Float.MAX_VALUE;
+//        float minFloat = Float.MIN_VALUE;
+//
+//        System.out.println("max float number is " + maxFloat);
+//        System.out.println("min float number is " + minFloat);
+//
+//        double maxValue = Double.MAX_VALUE;
+//        double minValue = Double.MIN_VALUE;
+//
+//        System.out.println("Double max " + maxValue);
+//        System.out.println("Double min " + minValue);
+
+        char letter = 'A';
+
+        System.out.println(letter);
+
+        boolean isActive = true;
+
+        System.out.println(isActive);
+
+
+    }
+}
