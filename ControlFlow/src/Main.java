d0c3e262a1631eb81d95db2bbc26090a203c48e8
@kammopajhb024 <mmopanemoroka@gmail.com>
2024-12-18 15:20:34 +0200

    if statements

---
+++ ControlFlow/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        int age = 16;
+//
+//        if(age >= 18) {
+//            System.out.println("You can vote");
+//            System.out.println("Please put X in ballot");
+//        } else {
+//            System.out.println("You can't vote");
+//            System.out.println("Please come back in " + (18 - age) + " years");
+//        }
+
+        int year = 2022;
+
+        if (year == 2019) {
+            System.out.println("the year is " + year);
+        } else if (year == 2020) {
+            System.out.println("the year is " + year);
+        } else if ( year == 2022) {
+            System.out.println("the year is " + year);
+        } else {
+            System.out.println("all conditions failed");
+        }
+    }
+}
