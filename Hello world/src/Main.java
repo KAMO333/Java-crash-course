f19e87efde03c28b27b6ef2b17b440657156dc7b
@kammopajhb024 <mmopanemoroka@gmail.com>
2024-12-18 14:42:12 +0200

    ops

--- Hello world/src/Main.java
+++ Hello world/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-
-        for (int i = 2; i < 10; i = i*i) {
-            System.out.println(i);
-        }
+        System.out.println("Hello World!");
     }
 }
