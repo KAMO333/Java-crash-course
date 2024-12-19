c88903df795c945f8371cf80c13b34426c285c22
@kammopajhb024 <mmopanemoroka@gmail.com>
2024-12-19 16:52:16 +0200

    playing with if's else

--- ControlFlow/src/Main.java
+++ ControlFlow/src/Main.java
@@ -1,26 +1,42 @@
 public class Main {
     public static void main(String[] args) {
 
-//        int age = 16;
+        int age = 17;
+
+        if(age >= 18) {
+            System.out.println("You can vote");
+            System.out.println("Please put X in ballot");
+        } else {
+            if (18 - age == 1) {
+                System.out.println("You can't vote");
+                System.out.println("Please come back in " + (18 - age) + " year");
+            } else {
+                System.out.println("You can't vote");
+                System.out.println("Please come back in " + (18 - age) + " years");
+            }
+
+        }
+
+//        int year = 2022;
 //
-//        if(age >= 18) {
-//            System.out.println("You can vote");
-//            System.out.println("Please put X in ballot");
+//        if (year == 2019) {
+//            System.out.println("the year is " + year);
+//        } else if (year == 2020) {
+//            System.out.println("the year is " + year);
+//        } else if ( year == 2022) {
+//            System.out.println("the year is " + year);
 //        } else {
-//            System.out.println("You can't vote");
-//            System.out.println("Please come back in " + (18 - age) + " years");
+//            System.out.println("all conditions failed");
 //        }
 
-        int year = 2022;
+//        boolean isActive = true;
+//        int number = 10;
+//
+//        if (!(number < 20)) {
+//            System.out.println("Active user");
+//        } else {
+//            System.out.println("User not active");
+//        }
 
-        if (year == 2019) {
-            System.out.println("the year is " + year);
-        } else if (year == 2020) {
-            System.out.println("the year is " + year);
-        } else if ( year == 2022) {
-            System.out.println("the year is " + year);
-        } else {
-            System.out.println("all conditions failed");
-        }
     }
 }
