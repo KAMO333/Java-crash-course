a3988793a9160b3411992140d9dd30bf8657693b
@kammopajhb024 <mmopanemoroka@gmail.com>
2024-12-19 17:15:50 +0200

    playing with if's else

--- ControlFlow/src/Main.java
+++ ControlFlow/src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int age = 17;
-
-        if(age >= 18) {
-            System.out.println("You can vote");
-            System.out.println("Please put X in ballot");
-        } else {
-            if (18 - age == 1) {
-                System.out.println("You can't vote");
-                System.out.println("Please come back in " + (18 - age) + " year");
-            } else {
-                System.out.println("You can't vote");
-                System.out.println("Please come back in " + (18 - age) + " years");
-            }
-
-        }
+//        int age = 17;
+//
+//        if(age >= 18) {
+//            System.out.println("You can vote");
+//            System.out.println("Please put X in ballot");
+//        } else {
+//            if (18 - age == 1) {
+//                System.out.println("You can't vote");
+//                System.out.println("Please come back in " + (18 - age) + " year");
+//            } else {
+//                System.out.println("You can't vote");
+//                System.out.println("Please come back in " + (18 - age) + " years");
+//            }
+//
+//        }
 
 //        int year = 2022;
 //
@@ -29,8 +29,8 @@ public class Main {
 //            System.out.println("all conditions failed");
 //        }
 
-//        boolean isActive = true;
-//        int number = 10;
+//        boolean isActive = false;
+//        int score = 100;
 //
 //        if (!(number < 20)) {
 //            System.out.println("Active user");
@@ -38,5 +38,18 @@ public class Main {
 //            System.out.println("User not active");
 //        }
 
+//        if (score >= 100 && isActive == true) {
+//            System.out.println("You opened the next level");
+//        } else {
+//            System.out.println("same level");
+//        }
+//     int num1 = -1;
+//     int num2 = 0;
+//
+//     if (num1 > 0 || num2 > 0) {
+//         System.out.println("True condition");
+//     } else {
+//         System.out.println("False conditions");
+//     }
     }
 }
